20dc902f6744b6ad809b81b2b0acb56dedfce8e3
jxy367 <jxy367@case.edu>
2018-07-08 20:52:40 -0700

    Added modifier aspect of roll function

--- GuuBot.py
+++ GuuBot.py
@@ -395,7 +395,7 @@ async def on_message(message):
             message_string = message.content
             modifier_matching = re.compile('roll \d+d\d+ *[-+]\d*', re.IGNORECASE)
             modifier_match = modifier_matching.match(message_string)
-            find_digits = re.compile('r\d+')
+            find_digits = re.compile('\d+')
             if modifier_match is None:
                 # Try basic matching
                 basic_matching = re.compile('roll \d+d\d+', re.IGNORECASE)
