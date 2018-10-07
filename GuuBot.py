ec8f31b542783d7861d9196e9c005eb30bfd4a09
J Wilk <ankq271@gmail.com>
2018-10-06 21:42:12 -0700

    Testing

--- GuuBot.py
+++ GuuBot.py
@@ -12,10 +12,20 @@ from bs4 import BeautifulSoup
 from discord.ext import commands
 from pytz import timezone
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 TOKEN = os.environ.get('TOKEN')
+GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROM_BIN')
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH')
 
 client = commands.Bot(command_prefix="guubot ", case_insensitive=True)
+
+# Configure Chromedriver
+chrome_options = Options()
+chrome_options.binary_location = GOOGLE_CHROME_BIN
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 # Guu Messages #
 woo = 'Woo'
